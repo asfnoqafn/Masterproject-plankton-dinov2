77ef79d1c6834ae2c9639e7a19c0228e987c7c0d
jeromel05 <jeromeluscher@gmail.com>
2024-07-08 16:38:07 +0200

    fix

--- dinov2/data/collate.py
+++ dinov2/data/collate.py
@@ -33,13 +33,18 @@ def collate_cpu(
     n_local_crops = len(samples[0][0]["local_crops"])
 
     n_gc, c, np, p = samples[0][0]["global_crops"].shape
-    coll_global_crops = [s[0]["global_crops"][i] for i in range(n_global_crops) for s in samples]
-    coll_local_crops = [s[0]["local_crops"][i] for i in range(n_local_crops) for s in samples]
+    coll_global_crops = [
+        s[0]["global_crops"][i] for i in range(n_global_crops) for s in samples
+    ]
+    coll_local_crops = [
+        s[0]["local_crops"][i] for i in range(n_local_crops) for s in samples
+    ]
 
     if do_free_shapes:
         c = coll_global_crops[0].size(0)
         coll_global_crops = [
-            rearrange(el, "c n p -> n c p", p=patch_size, c=c) for el in coll_global_crops
+            rearrange(el, "c n p -> n c p", p=patch_size, c=c)
+            for el in coll_global_crops
         ]  # var dim has to be first for pad sequences
         coll_global_crops = pad_sequence(coll_global_crops, batch_first=True)
         coll_global_crops = rearrange(
@@ -69,8 +74,12 @@ def collate_cpu(
         num_ch_list = [crop.shape[0] for crop in coll_global_crops]
         # b [c, np, p]
 
-        coll_global_crops = torch.cat(coll_global_crops)[:, None, :, :]  # (b n_gc c) 1 np p
-        coll_local_crops = torch.cat(coll_global_crops)[:, None, :, :]  # (b n_lc c) 1 np p
+        coll_global_crops = torch.cat(coll_global_crops)[
+            :, None, :, :
+        ]  # (b n_gc c) 1 np p
+        coll_local_crops = torch.cat(coll_local_crops)[
+            :, None, :, :
+        ]  # (b n_lc c) 1 np p
 
     else:
         coll_global_crops = torch.stack(coll_global_crops)
@@ -208,7 +217,9 @@ def collate_data_and_cast(
                 coll_global_crops[i].shape[-1] // patch_size,
             ) < random.uniform(prob_min, prob_max)
         else:
-            mask_tensor = torch.BoolTensor(mask_generator(int(N * random.uniform(prob_min, prob_max))))
+            mask_tensor = torch.BoolTensor(
+                mask_generator(int(N * random.uniform(prob_min, prob_max)))
+            )
         masks_list.append(mask_tensor)
         upperbound += int(N * prob_max)
     for i in range(n_samples_masked, B):
@@ -226,15 +237,23 @@ def collate_data_and_cast(
             masks_list.append(torch.BoolTensor(mask_generator(0)))
 
     if not do_free_shapes:
-        random.shuffle(masks_list)  # not possible if global crops of diff sizes in batch
+        random.shuffle(
+            masks_list
+        )  # not possible if global crops of diff sizes in batch
 
     # masks are dim: (gc_size // patch_size) ** 2, usually: (16 16)
-    collated_masks = torch.stack(masks_list).flatten(1)  # ((b nc) 16 16) -> ((b nc) 256)
+    collated_masks = torch.stack(masks_list).flatten(
+        1
+    )  # ((b nc) 16 16) -> ((b nc) 256)
     if use_ch_patch_embed:
         collated_masks = collated_masks.tile((1, c))  # ((b nc) 256) -> ((b nc) (c 256))
     mask_indices_list = collated_masks.flatten().nonzero().flatten()
 
-    masks_weight = (1 / collated_masks.sum(-1).clamp(min=1.0)).unsqueeze(-1).expand_as(collated_masks)[collated_masks]
+    masks_weight = (
+        (1 / collated_masks.sum(-1).clamp(min=1.0))
+        .unsqueeze(-1)
+        .expand_as(collated_masks)[collated_masks]
+    )
     if mask_indices_list.shape[0] > upperbound:
         upperbound = mask_indices_list.shape[0]
 
