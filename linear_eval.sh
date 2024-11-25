89c4bed73e1f8419b797e2f3188682e6d2f7e061
nicklechtenboerger <nicklechtenborger@gmail.com>
2024-11-25 10:13:21 +0100

    This version runs knn.py but fucks over train.py

--- linear_eval.sh
+++ linear_eval.sh
@@ -11,9 +11,9 @@ N_CPUS=19
 echo $SLURM_JOB_ID
 
 PYTHONPATH=/home/hk-project-p0021769/hgf_grc7525/Masterproject-plankton-dinov2 torchrun \
- --standalone --nnodes=1 Masterproject-plankton-dinov2/dinov2/run/eval/knn.py \
+ --standalone --nnodes=1 Masterproject-plankton-dinov2/dinov2/run/eval/linear.py \
  --config-file Masterproject-plankton-dinov2/dinov2/configs/eval/vits14_pretrain.yaml \
- --pretrained-weights ' /home/hk-project-p0021769/hgf_twg7490/output/2777745_1gpu_pre_21112024_190511_vit_small/model_0011249.rank_0.pth' --output-dir \
+ --pretrained-weights 'checkpoints/dinov2_vits14_pretrain.pth' --output-dir \
  /home/hk-project-p0021769/hgf_grc7525/output/ \
  --train-dataset="LMDBDataset:split=TRAIN:root=/home/hk-project-p0021769/hgf_grc7525/workspace/hkfswork/hgf_grc7525-nick/plankton:extra=*" \
  --val-dataset="LMDBDataset:split=VAL:root=/home/hk-project-p0021769/hgf_grc7525/workspace/hkfswork/hgf_grc7525-nick/plankton/:extra=*" \
