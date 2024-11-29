dc566e7433197abe8181cfc3cd154f24424d410d
nicklechtenboerger <nicklechtenborger@gmail.com>
2024-11-29 12:42:36 +0100

    cipics sh

---
+++ cpics.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH -e /home/hk-project-p0021769/hgf_grc7525/output/log_%j.err
+#SBATCH --output /home/hk-project-p0021769/hgf_grc7525/output/log_%j.out
+#SBATCH --time 00:40:00
+#SBATCH --partition=dev_cpuonly
+
+PYTHONPATH=/home/hk-project-p0021769/hgf_grc7525/Masterproject-plankton-dinov2
+torchrun Masterproject-plankton-dinov2/dinov2/data/dataset_creation/save_cpics_to_lmdb.py\
+ --dataset_path="/home/hk-project-p0021769/hgf_grc7525/data/with_labels/UVPEC/" \
+ --lmdb_dir_name="/home/hk-project-p0021769/hgf_grc7525/data/lmdb_with_labels/" \
+ --min_size="0" --dataset_name="UVPEC"
---
+++ pca.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#SBATCH --gres=gpu:2
+#SBATCH --nodes=1
+#SBATCH --cpus-per-gpu=19
+#SBATCH -e /home/hk-project-p0021769/hgf_grc7525/output/log_%j.err
+#SBATCH --output /home/hk-project-p0021769/hgf_grc7525/output/log_%j.out
+#SBATCH --time 00:40:00
+#SBATCH --partition=dev_accelerated
+N_GPUS=2
+N_CPUS=19
+echo $SLURM_JOB_ID
+
+NUM_COMPONENTS=50
+PYTHONPATH=/home/hk-project-p0021769/hgf_grc7525/Masterproject-plankton-dinov2 torchrun \
+ --standalone --nnodes=1 Masterproject-plankton-dinov2/dinov2/eval/pca.py \
+ --num-components=$NUM_COMPONENTS \
+ --output-dir='/home/hk-project-p0021769/hgf_grc7525/output/' \
+ --train-dataset="LMDBDataset:split=TRAIN:root=/home/hk-project-p0021769/hgf_grc7525/workspace/hkfswork/hgf_grc7525-nick/plankton:extra=*" \
+ --val-dataset="LMDBDataset:split=VAL:root=/home/hk-project-p0021769/hgf_grc7525/workspace/hkfswork/hgf_grc7525-nick/plankton/:extra=*" \
+ --batch-size=256 \
