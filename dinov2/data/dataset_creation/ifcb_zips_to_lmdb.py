6e266977acbf35f975fe285f0a0e32dc0780c4cc
Johann Schulze Tast <johann.schulzetast@student.hpi.de>
2024-12-10 15:57:40 +0100

    add lmdb creation script for IFCB zips & and IFCB/WHOI dataset scraping
    Co-Authored-By: Oliver Schulz <64468276+olschulz@users.noreply.github.com>

---
+++ dinov2/data/dataset_creation/ifcb_zips_to_lmdb.py
@@ -0,0 +1,216 @@
+import argparse
+import json
+import os
+import sys
+from zipfile import ZipFile
+
+import imageio.v3 as iio
+import lmdb
+import numpy as np
+from tqdm import tqdm
+
+MAP_SIZE_IMG = int(1e12)  # 1TB
+MAP_SIZE_META = int(1e8)  # 100MB
+
+
+def normalize(x):
+    return (x - x.min()) / (x.max() - x.min() + 1e-5)
+
+
+def load_img(img_path):
+    img = iio.imread(img_path)  # (N M)
+
+    height, width = img.shape[:2]
+
+    # Check if the image is smaller than the minimum size
+    # probably should allow if only one dimension is smaller thin algae
+    if height < args.min_size and width < args.min_size:
+        raise ValueError(
+            f"Image is smaller than {args.min_size}x{args.min_size}"
+        )
+
+    # disallow one dimensional images
+    if height < 2 or width < 2:
+        raise ValueError("Image is one dimensional.")
+
+    img = normalize(np.squeeze(img))
+    img = (img * 255).astype(np.uint8)
+    return img
+
+
+def get_all_zips(ifcb_path, processed_bins):
+    zips = []
+    for zip_filename in os.listdir(ifcb_path):
+        if (
+            zip_filename.endswith(".zip")
+            and zip_filename not in processed_bins
+        ):
+            zips.append(zip_filename)
+    return zips
+
+
+def build_lmdbs(args):
+    lmdb_dir = os.path.abspath(args.lmdb_dir_name)
+    processed_bins_path = os.path.join(
+        lmdb_dir, "processed_bins.json"
+    )
+    number_of_filtered_images = 0
+    total_images_in_chunk = 0
+    try:
+        processed_bins_dir = json.load(
+            open(
+                processed_bins_path,
+                "r",
+            )
+        )
+    except (json.JSONDecodeError, FileNotFoundError):
+        processed_bins_dir = {
+            "processed_bins": list(),
+            "total_images": 0,
+        }
+    finally:
+        processed_bins = processed_bins_dir[
+            "processed_bins"
+        ]
+        total_images = processed_bins_dir["total_images"]
+        prior_total_images = processed_bins_dir[
+            "total_images"
+        ]
+
+    zips = get_all_zips(args.ifcb_path, processed_bins)
+
+    lmdb_imgs_path = os.path.join(
+        lmdb_dir,
+        f"startid_{total_images}_images",
+    )
+    os.makedirs(lmdb_imgs_path, exist_ok=True)
+
+    env = lmdb.open(lmdb_imgs_path, map_size=MAP_SIZE_IMG)
+    txn = env.begin(write=True)
+    for zip_filename in zips:
+        # open next zip-file and wirte images to lmdb
+        zip_filepath = os.path.join(
+            args.ifcb_path, zip_filename
+        )
+        with ZipFile(zip_filepath) as zf:
+            for image_relpath in tqdm(zf.namelist()):
+                if "__MACOSX" in image_relpath:
+                    continue
+                if image_relpath.endswith(".png"):
+                    image_path = os.path.join(
+                        zip_filepath, image_relpath
+                    )
+
+                    try:
+                        image = load_img(
+                            zf.read(image_relpath)
+                        )
+                    except Exception as e:
+                        print(
+                            f"Error loading image {image_path}: {e}. Skipping...",
+                            file=sys.stderr,
+                        )
+                        number_of_filtered_images += 1
+                        continue
+
+                    img_key = os.path.join(
+                        zip_filename, image_relpath
+                    ).replace(
+                        "/", "_"
+                    )  # Replace slashes for safety
+                    img_key_bytes = img_key.encode("utf-8")
+
+                    # Load and encode the image
+                    img_encoded = iio.imwrite(
+                        "<bytes>", image, extension=".png"
+                    )
+
+                    # Save to LMDB
+                    txn.put(img_key_bytes, img_encoded)
+                    total_images += 1
+                    total_images_in_chunk += 1
+        processed_bins.append(
+            zip_filename
+        )  # bin is processed, so add it to processed list
+        if (
+            total_images_in_chunk >= args.chunk_size
+            or zip_filename == zips[-1]
+        ):
+            # close full lmdb
+            txn.commit()
+            env.close()
+            print(
+                f"Finished importing from {args.ifcb_path} and subdirectories, saved at: {lmdb_imgs_path}"
+            )
+            lmdb_imgs_path = os.path.join(
+                lmdb_dir,
+                f"startid_{total_images}_images",
+            )
+            total_images_in_chunk = 0
+            # dump json, since all imgs of processed bins are saved to lmdb
+            processed_bins_dir["processed_bins"] = (
+                processed_bins
+            )
+            processed_bins_dir["total_images"] = (
+                total_images
+            )
+            json.dump(
+                processed_bins_dir,
+                open(
+                    processed_bins_path,
+                    "w",
+                ),
+            )
+
+            if zip_filename != zips[-1]:
+                # open new lmdb
+                os.makedirs(lmdb_imgs_path, exist_ok=True)
+                env = lmdb.open(
+                    lmdb_imgs_path, map_size=MAP_SIZE_IMG
+                )
+                txn = env.begin(write=True)
+
+    if number_of_filtered_images > 0:
+        print(
+            f"Filtered {number_of_filtered_images} images"
+        )
+
+    print(
+        f"TOTAL #images {total_images - prior_total_images} FROM {args.ifcb_path}"
+    )
+
+
+def get_args_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--ifcb_path",
+        type=str,
+        help="""Path to ifcb folder with bins to process.""",
+        default="/home/hk-project-p0021769/hgf_vwg6996/data/ifcb",
+    )
+    parser.add_argument(
+        "--lmdb_dir_name",
+        type=str,
+        help="Base lmdb dir name",
+        default="ifcb",
+    )
+    parser.add_argument(
+        "--min_size",
+        type=int,
+        help="Minimum image size (width and height)",
+        default=0,
+    )
+    parser.add_argument(
+        "--chunk_size",
+        type=int,
+        help="Size to chunk images into different lmdbs",
+        default=10_000_000,
+    )
+
+    return parser
+
+
+if __name__ == "__main__":
+    args_parser = get_args_parser()
+    args = args_parser.parse_args()
+    sys.exit(build_lmdbs(args))
