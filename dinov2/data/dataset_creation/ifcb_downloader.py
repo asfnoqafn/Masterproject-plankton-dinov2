25534b7b64391e86e17c3de2a7f836e9fb1b9a86
Johann Schulze Tast <johann.schulzetast@student.hpi.de>
2024-12-18 09:43:05 +0100

    Improved API failure handling of ifcp_downloader.py
    Co-Authored-By: Oliver Schulz <64468276+olschulz@users.noreply.github.com>

--- dinov2/data/dataset_creation/ifcb_downloader.py
+++ dinov2/data/dataset_creation/ifcb_downloader.py
@@ -7,6 +7,7 @@ from pathlib import Path
 import csv
 import threading
 import logging
+from typing import Optional
 
 import requests
 
@@ -15,7 +16,7 @@ import requests
 state_lock = threading.Lock()
 
 # "complete crap" according to mvco csv, even though they don't have the correct tags, so we skip these
-blacklisted_mvco_bins = ["D20211123T063204_IFCB010","D20211123T065325_IFCB010","D20211123T071429_IFCB010","D20211123T073533_IFCB010","D20211123T075637_IFCB010","D20211123T081741_IFCB010","D20211123T083845_IFCB010","D20211123T085949_IFCB010","D20211123T092053_IFCB010","D20211123T094157_IFCB010","D20211123T100301_IFCB010","D20211123T102405_IFCB010","D20211123T104509_IFCB010","D20211123T110613_IFCB010","D20211123T112717_IFCB010","D20211123T114821_IFCB010","D20211123T120925_IFCB010","D20211123T123029_IFCB010","D20211123T125133_IFCB010","D20211123T131237_IFCB010","D20211123T133341_IFCB010","D20211123T135445_IFCB010","D20211123T141550_IFCB010","D20211123T143654_IFCB010","D20211123T145758_IFCB010","D20211123T151902_IFCB010","D20211123T154007_IFCB010","D20211123T160111_IFCB010","D20211123T162215_IFCB010","D20211123T164320_IFCB010","D20211123T170424_IFCB010","D20211123T172528_IFCB010","D20211123T174632_IFCB010","D20211123T180736_IFCB010","D20211123T182840_IFCB010","D20211123T184944_IFCB010","D20211123T191048_IFCB010","D20211123T193153_IFCB010","D20211123T195257_IFCB010","D20211123T201401_IFCB010","D20211123T203505_IFCB010","D20211123T205610_IFCB010","D20211123T211714_IFCB010","D20211123T213818_IFCB010","D20211123T215922_IFCB010","D20211123T222026_IFCB010","D20211123T224131_IFCB010","D20211123T230235_IFCB010","D20211123T232338_IFCB010","D20211123T234442_IFCB010","D20211124T000546_IFCB010","D20211124T002650_IFCB010","D20211124T004754_IFCB010","D20211124T010858_IFCB010","D20211124T013002_IFCB010","D20211124T015106_IFCB010","D20211124T021210_IFCB010","D20211124T023314_IFCB010","D20211124T025418_IFCB010","D20211124T031522_IFCB010","D20211124T033626_IFCB010","D20211124T035730_IFCB010","D20211124T041834_IFCB010","D20211124T043938_IFCB010","D20211124T050042_IFCB010","D20211124T054310_IFCB010"]
+blacklisted_mvco_bins = ["D20211123T063204_IFCB010", "D20211123T065325_IFCB010", "D20211123T071429_IFCB010", "D20211123T073533_IFCB010", "D20211123T075637_IFCB010", "D20211123T081741_IFCB010", "D20211123T083845_IFCB010", "D20211123T085949_IFCB010", "D20211123T092053_IFCB010", "D20211123T094157_IFCB010", "D20211123T100301_IFCB010", "D20211123T102405_IFCB010", "D20211123T104509_IFCB010", "D20211123T110613_IFCB010", "D20211123T112717_IFCB010", "D20211123T114821_IFCB010", "D20211123T120925_IFCB010", "D20211123T123029_IFCB010", "D20211123T125133_IFCB010", "D20211123T131237_IFCB010", "D20211123T133341_IFCB010", "D20211123T135445_IFCB010", "D20211123T141550_IFCB010", "D20211123T143654_IFCB010", "D20211123T145758_IFCB010", "D20211123T151902_IFCB010", "D20211123T154007_IFCB010", "D20211123T160111_IFCB010", "D20211123T162215_IFCB010", "D20211123T164320_IFCB010", "D20211123T170424_IFCB010", "D20211123T172528_IFCB010", "D20211123T174632_IFCB010", "D20211123T180736_IFCB010", "D20211123T182840_IFCB010", "D20211123T184944_IFCB010", "D20211123T191048_IFCB010", "D20211123T193153_IFCB010", "D20211123T195257_IFCB010", "D20211123T201401_IFCB010", "D20211123T203505_IFCB010", "D20211123T205610_IFCB010", "D20211123T211714_IFCB010", "D20211123T213818_IFCB010", "D20211123T215922_IFCB010", "D20211123T222026_IFCB010", "D20211123T224131_IFCB010", "D20211123T230235_IFCB010", "D20211123T232338_IFCB010", "D20211123T234442_IFCB010", "D20211124T000546_IFCB010", "D20211124T002650_IFCB010", "D20211124T004754_IFCB010", "D20211124T010858_IFCB010", "D20211124T013002_IFCB010", "D20211124T015106_IFCB010", "D20211124T021210_IFCB010", "D20211124T023314_IFCB010", "D20211124T025418_IFCB010", "D20211124T031522_IFCB010", "D20211124T033626_IFCB010", "D20211124T035730_IFCB010", "D20211124T041834_IFCB010", "D20211124T043938_IFCB010", "D20211124T050042_IFCB010", "D20211124T054310_IFCB010"]
 
 
 def download_bin(bin: tuple[str, str, str, int], api_path: str, output_dir, include_features=True, include_bin_metadata=True):
@@ -43,16 +44,17 @@ def download_bin(bin: tuple[str, str, str, int], api_path: str, output_dir, incl
 
             # Extract filename from the URL or headers
             filename = url_and_path.split("/")[-1]
-            output_path = os.path.join(output_dir,filename)
+            output_path = os.path.join(output_dir, filename)
+            download_path = f"{output_path}.download"
 
             response = requests.get(url_and_path, stream=True, timeout=60)
             response.raise_for_status()
 
             # Save the file to disk
-            with open(output_path, 'wb') as f:
+            with open(download_path, 'wb') as f:
                 for chunk in response.iter_content(chunk_size=8192):
                     f.write(chunk)
-
+            os.rename(download_path, output_path)
         except requests.HTTPError as e:
             if file_to_download == "_features.csv" and response.status_code == 404:
                 # features file is not available for all bins
@@ -79,14 +81,19 @@ def download_bin(bin: tuple[str, str, str, int], api_path: str, output_dir, incl
                 data["last_bin"] = bin_id
             f.seek(0)
             json.dump(data, f)
-    
+
     if zip_failed:
         raise failed_files[0][1]
 
     return failed_files
 
 
-def get_bin_data(csv_path, output_dir: str, max_bins: int | None = None, start_bin: str | None = None, blacklisted_tags: list[str] = [], blacklisted_sample_types: list[str] = []):
+def force_download_bin_list(bin_list: list[str], api_path: str, output_dir, include_features=True, include_bin_metadata=True):
+    for bin in bin_list:
+        download_bin(("", bin, "", 0), api_path, output_dir, include_features, include_bin_metadata)
+
+
+def get_bin_data(csv_path, output_dir: str, max_bins: Optional[int] = None, start_bin: Optional[str] = None, blacklisted_tags: list[str] = [], blacklisted_sample_types: list[str] = []):
     """
     Get the binary data from a CSV file.
 
@@ -99,17 +106,19 @@ def get_bin_data(csv_path, output_dir: str, max_bins: int | None = None, start_b
     bins: list[tuple[str, str, str, int]] = []
     blacklisted_bins = []
     logging.info(f"Reading CSV file from {csv_path}")
+    logging.info(f"Blacklisted tags: {blacklisted_tags}")
+    logging.info(f"Blacklisted sample types: {blacklisted_sample_types}")
     with open(csv_path, 'r') as file:
         csv_reader = csv.DictReader(file, delimiter=',', quotechar='"')
         # data cleaning
-        for i,line in enumerate(csv_reader):
+        for i, line in enumerate(csv_reader):
             # dataset, pid, sample_time, ifcb, ml_analyzed, latitude, longitude, depth, cruise, cast, niskin, sample_type, n_images, tag1, tag2, tag3, tag4, comment_summary, trigger_selection, skip
-            if line["dataset"]  == "mvco":
+            if line["dataset"] == "mvco":
                 all_tags: list[str] = []
                 for tags in [line["tag1"], line["tag2"], line["tag3"], line["tag4"]]:
                     all_tags.extend(tags.split(","))
                 all_tags = [tag.strip().lower() for tag in all_tags if tag != ""]
-                if line["skip"] == "1" or any(tag in blacklisted_tags for tag in all_tags) or line["sample_type"] != "" and line["sample_type"] in blacklisted_sample_types or line["pid"] in blacklisted_mvco_bins:
+                if line["skip"] == "1" or any(tag in blacklisted_tags for tag in all_tags) or line["sample_type"] in blacklisted_sample_types or line["pid"] in blacklisted_mvco_bins:
                     blacklisted_bins.append((line["sample_time"], line["pid"], line["dataset"], int(line["n_images"])))
                     continue
                 bins.append((line["sample_time"], line["pid"], line["dataset"], int(line["n_images"])))
@@ -149,7 +158,7 @@ def download_multiple_zips(bins, api_path, output_dir, max_workers=5):
     # Use ThreadPoolExecutor for concurrent downloading
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
         # Map each URL to the download_zip function
-        future_to_bin = {executor.submit(download_bin, bin=bin, api_path=api_path, output_dir=output_dir ): bin for bin in bins}
+        future_to_bin = {executor.submit(download_bin, bin=bin, api_path=api_path, output_dir=output_dir): bin for bin in bins}
 
         # Process completed downloads
         for future in concurrent.futures.as_completed(future_to_bin):
@@ -168,7 +177,7 @@ def download_multiple_zips(bins, api_path, output_dir, max_workers=5):
 
 def main(args):
     os.makedirs(args.output_dir, exist_ok=True)
-    bins = get_bin_data(args.csv_path, output_dir=args.output_dir, start_bin=args.start_bin, blacklisted_sample_types=args.blacklisted_sample_types, blacklisted_tags=args.blacklisted_tags, max_bins=args.max_bins )  # gets all bins not present in the folder
+    bins = get_bin_data(args.csv_path, output_dir=args.output_dir, start_bin=args.start_bin, blacklisted_sample_types=args.blacklisted_sample_types.split(","), blacklisted_tags=args.blacklisted_tags.split(","), max_bins=args.max_bins)  # gets all bins not present in the folder
 
     download_multiple_zips(bins=bins, api_path=args.api_path, output_dir=args.output_dir, max_workers=args.num_workers)
 
