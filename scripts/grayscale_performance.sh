1f831be3f0ddf374f7d5d4306eb14ef8bbd93da0
nicklechtenboerger <nicklechtenborger@gmail.com>
2025-01-15 22:52:46 +0100

    performance testing

--- scripts/grayscale_performance.sh
+++ scripts/grayscale_performance.sh
@@ -6,7 +6,7 @@
 #SBATCH -e /home/hk-project-p0021769/hgf_grc7525/repo/output/log_%j.err
 #SBATCH --output /home/hk-project-p0021769/hgf_grc7525/repo/output/log_%j.out
 #SBATCH --time 00:45:00
-#SBATCH --partition=accelerated-h100
+#SBATCH --partition=dev_accelerated-h100
 
 BATCH_S=128
 N_GPUS=4
@@ -21,6 +21,6 @@ OMP_NUM_THREADS=128 PYTHONPATH=/home/hk-project-p0021769/hgf_grc7525/repo/Master
 	--standalone --nnodes=1 --nproc_per_node=$N_GPUS /home/hk-project-p0021769/hgf_grc7525/repo/Masterproject-plankton-dinov2/dinov2/train/train.py --no-resume \
 	--config-file /home/hk-project-p0021769/hgf_grc7525/repo/Masterproject-plankton-dinov2/dinov2/configs/train/whoi.yaml --run_name=${SLURM_JOB_ID}_${N_GPUS}gpu_pre \
 	train.output_dir='/home/hk-project-p0021769/hgf_grc7525/output/' train.use_torch_compile=true \
-	train.dataset_path=LMDBDataset:split=ALL:root=/hkfs/work/workspace/scratch/hgf_grc7525-nick/train/nontest/:extra=* train.num_workers=$N_CPUS \
+	train.dataset_path=LMDBDataset:split=ALL:root=/home/hk-project-p0021769/hgf_grc7525/workspace/hkfswork/hgf_grc7525-nick/test/lmdb_with_labels/:extra=* train.num_workers=$N_CPUS \
 	train.batch_size_per_gpu=$BATCH_S train.augmentations=kornia_cpu student.arch=vit_small crops.use_native_res=false crops.free_shapes=none \
 	crops.use_ch_patch_embed=false train.in_chans=1 optim.base_lr=0.001 crops.use_variable_channels=false
